549f24351696986d7b7d4d6c44f40f3d087cc5bc
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-06-08 14:43:22 -0400

    Modificación en index.js: funciones

--- index.js
+++ index.js
@@ -23,24 +23,25 @@ const wrapper = document.getElementById("wrapper");
 const cancel = document.getElementById("cancel");
 const quantity = document.getElementById("value");
 const total = document.getElementById("total");
+const totalPayProducts = document.getElementById("totalPayProducts");
 const follow = document.getElementById("continue");
 const init = document.getElementById("init");
 const checkPay = document.getElementById("block");
 const purchaseTicket = document.getElementById("purchaseTicket");
 const ocult = document.getElementById("ocult");
 const totalPay = document.getElementById("invisible");
-const totalPayProducts = document.getElementById("totalPayProducts");
+const returnProduct = document.getElementById("returnProduct");
 const unit = document.getElementById("unit");
 const namePay = document.getElementById("namePay");
 const acount = document.getElementById("acount");
 const descriptionProduct = document.getElementById("descriptionProduct");
 const finish = document.getElementById("finish");
-const print= document.getElementById("print");
+const print = document.getElementById("print");
 /*Declaración de variables*/
-let shelf;
 let sum;
 let priceProduct;
 let state = "";
+let output = "";
 let cont = 0;
 
 /*Definición del estado inicial de las pantallas creadas(ocultadas o no ocultadas)*/
@@ -57,186 +58,222 @@ buyProduct3.addEventListener("click", detailsBuyProduct);
 cancel.addEventListener("click", cancelled);
 follow.addEventListener("click", buy);
 init.addEventListener("click", begin);
+returnProduct.addEventListener("click", returnBuyProduct);
 totalPay.addEventListener("click", totalCancel);
 finish.addEventListener("click", finishPay);
-print.addEventListener("click",voucherPrint);
+print.addEventListener("click", voucherPrint);
 
 /*Funciones asociadas a los eventos declarados anteriormente*/
 function buy() {
-    wrapper.style.display = "block";
-    aboutPage.style.display = "none";
-    buyDetails.style.display = "none";
+	wrapper.style.display = "block";
+	aboutPage.style.display = "none";
+	buyDetails.style.display = "none";
 }
 
 function detailsBuyProduct(e) {
 
-    let x = e.target.id;
+	let x = e.target.id;
 
-    if (x == "buy1") {
+	if (x == "buy1") {
 
-        aboutPage.style.display = "none";
-        price.innerHTML = "$1.000";
-        selectImg.src = "pencils.jpeg";
-        priceProduct = 1000;
-        buyDetails.style.display = "block";
+		aboutPage.style.display = "none";
+		price.innerHTML = "$1.000";
+		selectImg.src = "pencils.jpeg";
+		priceProduct = 1000;
+		buyDetails.style.display = "block";
 
-    }
+	}
 
-    if (x == "buy2") {
+	if (x == "buy2") {
 
-        aboutPage.style.display = "none";
-        titleProduct.innerHTML = " Lápiz Portaminas Unicornio 0.5 mm";
-        price.innerHTML = "$1.500";
-        selectImg.src = "pencilpony.jpeg";
-        priceProduct = 1500;
-        buyDetails.style.display = "block";
+		aboutPage.style.display = "none";
+		titleProduct.innerHTML = " Lápiz Portaminas Unicornio 0.5 mm";
+		price.innerHTML = "$1.500";
+		selectImg.src = "pencilpony.jpeg";
+		priceProduct = 1500;
+		buyDetails.style.display = "block";
 
-    }
+	}
 
-    if (x == "buy3") {
-        aboutPage.style.display = "none";
-        titleProduct.innerHTML = "Lápiz Tinta Gel Negro Osito 3 diseños";
-        price.innerHTML = "$600";
-        selectImg.src = "bearpencil.jpeg";
-        priceProduct = 600;
-        buyDetails.style.display = "block";
-    }
+	if (x == "buy3") {
+		aboutPage.style.display = "none";
+		titleProduct.innerHTML = "Lápiz Tinta Gel Negro Osito 3 diseños";
+		price.innerHTML = "$600";
+		selectImg.src = "bearpencil.jpeg";
+		priceProduct = 600;
+		buyDetails.style.display = "block";
+	}
 }
 
 function cancelled() {
-    wrapper.style.display = "none";
-    numberCard.value = "";
-    aboutPage.style.display = "block";
-    location.reload();
+	wrapper.style.display = "none";
+	numberCard.value = "";
+	aboutPage.style.display = "block";
+	location.reload();
 }
 
 function begin() {
-    aboutPage.style.display = "block";
-    buyDetails.style.display = "none";
-    location.reload();
+	aboutPage.style.display = "block";
+	buyDetails.style.display = "none";
+	location.reload();
+}
+
+function returnBuyProduct() {
+	wrapper.style.display = "none";
+	popUpWindow.style.display = "none";
+	buyDetails.style.display = "block";
+	state = "";
+	numberCard.value = "";
+	logoCreditCard.src = "";
+	iconTarjet.className = "";
 }
 
 function totalCancel() {
-    wrapper.style.display = "none";
-    unit.innerHTML = "Unidades : " + "                  " + quantity.value;
-    totalPayProducts.innerHTML = "Monto :" + "                    " + "$" + sum;
-    namePay.innerHTML = "Nombre :" + "           " + name.value.toUpperCase();
-    acount.innerHTML = "Cuenta : " + "           " + numberCard.value;
-    descriptionProduct.innerHTML = "Descripción :" + " " + titleProduct.innerHTML;
-    purchaseTicket.style.display = "block";
+	wrapper.style.display = "none";
+	unit.innerHTML = "Unidades : " + "                  " + quantity.value;
+	totalPayProducts.innerHTML = "Monto :" + "                    " + "$" + sum;
+	namePay.innerHTML = "Nombre :" + "           " + name.value.toUpperCase();
+	acount.innerHTML = "Cuenta : " + "           " + numberCard.value;
+	descriptionProduct.innerHTML = "Descripción :" + " " + titleProduct.innerHTML;
+	purchaseTicket.style.display = "block";
 }
 
 function finishPay() {
-    purchaseTicket.style.display = "none";
-    aboutPage.style.display = "block";
-    location.reload();
+	purchaseTicket.style.display = "none";
+	aboutPage.style.display = "block";
+	location.reload();
 }
 
-function voucherPrint(){
-    window.print();
+function voucherPrint() {
+	window.print();
 }
 
-quantity.onchange = function() {
-    sum = (quantity.value) * priceProduct;
-    total.innerHTML = "$" + sum;
+quantity.onchange = function () {
+	sum = (quantity.value) * priceProduct;
+	total.innerHTML = "$" + sum;
 }
 
 /*El evento desencadena que se evite ingresar números a la casilla de nombre en la pantalla de medio de pago*/
-name.onkeypress = function(e) {
-    let key = e.keyCode || e.which;
-    let computerKey = String.fromCharCode(key).toLowerCase();
-    let letters = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-    let character = "";
-    let especials = character >= 33 && character <= 47;
+name.onkeypress = function (e) {
+	let key = e.keyCode || e.which;
+	let computerKey = String.fromCharCode(key).toLowerCase();
+	let letters = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+	let character = "";
+	let especials = character >= 33 && character <= 47;
 
-    let especialKey = false;
-    for (let i in especials) {
-        if (key == especials[i]) {
-            especialKey = true;
-            break;
-        }
-    }
+	let especialKey = false;
+	for (let i in especials) {
+		if (key == especials[i]) {
+			especialKey = true;
+			break;
+		}
+	}
 
-    if (letters.indexOf(computerKey) == -1 && !especialKey) {
-        return false;
-    }
+	if (letters.indexOf(computerKey) == -1 && !especialKey) {
+		return false;
+	}
 
 }
 
 /*El evento desencadena que se evite ingresar letras a la casilla de númerode tarjeta en la pantalla de medio de pago*/
-numberCard.onkeypress = function(evt) {
-    let charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-    } else {
-        numberCard.onkeyup = function(e) {
-            shelf = e.currentTarget.value;
-            if (shelf[0] == 4 ){
-                logoCreditCard.src="";
-                iconTarjet.className="fab fa-cc-visa fa-2x";
-                iconTarjet.style.color="navy";
-            }else if ( shelf [0] == 5){
-                logoCreditCard.src="mastercard.png";
-                iconTarjet.className="";
-            }else if (shelf[0] == 3){
-                logoCreditCard.src="";
-                iconTarjet.className= "fab fa-cc-amex fa-2x";
-                iconTarjet.style.color="royalblue";
-            }else if (shelf[0]== 6){
-                logoCreditCard.src="discovercard.png";
-                iconTarjet.className="";
-            }else if (shelf[0]== 1){
-                logoCreditCard.src="redcompra.png";
-                iconTarjet.className="";
-            }
-            state = state + shelf.charAt(cont);
-            numberCard.value = validator.maskify(numberCard.value);
-            cont = cont + 1;
-        }
-        return true;
-    }
+numberCard.onkeypress = function (evt) {
+	let charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+		return false;
+	} else {
+		numberCard.onkeyup = function (e) {
+			let shelf = e.target.value;
+			let remove = 0;
+			let charCode1 = e.key;
+			let numbers = "1234567890";
+			if (charCode1 == "Backspace") {
+				remove = remove + 1;
+				if (numberCard.value == "") {
+					state = "";
+				} else {
+					state = state.slice(0, -remove);
+				}
+			}
+			if (numbers.indexOf(charCode1) == -1 || charCode1 == "Backspace") {
+				return false;
+			} else {
+				if (shelf[0] == 4) {
+					logoCreditCard.src = "";
+					iconTarjet.className = "fab fa-cc-visa fa-2x";
+					iconTarjet.style.color = "navy";
+				} else if (shelf[0] == 5) {
+					logoCreditCard.src = "mastercard.png";
+					iconTarjet.className = "";
+				} else if (shelf[0] == 3) {
+					logoCreditCard.src = "";
+					iconTarjet.className = "fab fa-cc-amex fa-2x";
+					iconTarjet.style.color = "royalblue";
+				} else if (shelf[0] == 6) {
+					logoCreditCard.src = "discovercard.png";
+					iconTarjet.className = "";
+				} else if (shelf[0] == 1) {
+					logoCreditCard.src = "redcompra.png";
+					iconTarjet.className = "";
+				}
+				output += e.key;
+				state = state + output.charAt(cont);
+				numberCard.value = validator.maskify(numberCard.value);
+				cont = cont + 1;
+			}
+			return true;
+		}
+
+	}
+
 }
 
 function validation() {
-    let cardNumber = state;
-    let text = validator.isValid(cardNumber);
-    if (text == false) {
-        popUpWindow.style.display = "block";
-        numberCard.value = "";
-        logoCreditCard.src="";
-        iconTarjet.className="";
-        state = "";
-    } else {
-        message.innerHTML = "Su tarjeta es válida. Puede continuar con su compra."
-        checkPay.className = "fas fa-check-circle fa-3x";
-        checkPay.style.color = "lightblue";
-        ocult.style.display = "block";
-        popUpWindow.style.display = "block";
-        state = "";
-    }
+	let cardNumber = state;
+	let text = validator.isValid(cardNumber);
+	if (text == false) {
+		message.innerHTML = "Su tarjeta no es válida.Vuelva a intentarlo o cancele su compra.";
+		checkPay.className = "fas fa-times fa-3x";
+		checkPay.style.color = "darkred";
+		ocult.style.display = "none";
+		popUpWindow.style.display = "block";
+		numberCard.value = "";
+		logoCreditCard.src = "";
+		iconTarjet.className = "";
+		state = "";
+	} else if (text == true) {
+		message.innerHTML = "Su tarjeta es válida. Puede continuar con su compra."
+		checkPay.className = "fas fa-check-circle fa-3x";
+		checkPay.style.color = "lightblue";
+		ocult.style.display = "block";
+		popUpWindow.style.display = "block";
+		state = "";
+	}
 }
 
 /*El evento desencadena que se evite ingresar letras a la casilla CVC en la pantalla de medio de pago*/
-cvc.onkeypress = function(e) {
-    let charCode = (e.which) ? e.which : e.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        return false;
-    } else {
-        return true;
-    }
+cvc.onkeypress = function (e) {
+	let charCode = (e.which) ? e.which : e.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
-send.onclick = function() {
-    check.onsubmit = function() {
-        validation();
-        return false;
-    }
+send.onclick = function () {
+	check.onsubmit = function () {
+		validation();
+		return false;
+	}
 }
 
 close.addEventListener("click", closeWindow);
 
 function closeWindow() {
-    popUpWindow.style.display = "none";
-    cvc.value = "";
-    state = "";
+	popUpWindow.style.display = "none";
+	cvc.value = "";
+	logoCreditCard.src = "";
+	iconTarjet.className = "";
+	numberCard.value = "";
+	state = "";
 }
